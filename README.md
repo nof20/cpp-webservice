127682b7b1bd245f9150eda31bdc84fc2c75f092
Nick Franklin <n.o.franklin@gmail.com>
2021-07-21 18:23:13 -0400

    Text and JSON responses now work

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # cpp-webservice
 
 A C++ webservice example using OatPP.
+
+```
+$ curl localhost:8000/hello
+Hello World!
+$ curl localhost:8000/helloJson
+{"statusCode":1024,"message":"Hello DTO!"}
+```
--- src/main.cc
+++ src/main.cc
@@ -1,14 +1,87 @@
+#include "oatpp/parser/json/mapping/ObjectMapper.hpp"
 #include "oatpp/web/server/HttpConnectionHandler.hpp"
-
 #include "oatpp/network/Server.hpp"
 #include "oatpp/network/tcp/server/ConnectionProvider.hpp"
+#include "oatpp/core/macro/codegen.hpp"
 
 // See https://oatpp.io/docs/start/step-by-step/
+
+/** 
+ * Custom Request Handler
+ */
+class Handler : public oatpp::web::server::HttpRequestHandler
+{
+public:
+    /**
+   * Handle incoming request and return outgoing response.
+   */
+    std::shared_ptr<OutgoingResponse> handle(const std::shared_ptr<IncomingRequest> &request) override
+    {
+        return ResponseFactory::createResponse(Status::CODE_200, "Hello World!");
+    }
+};
+
+/* Begin DTO code-generation */
+#include OATPP_CODEGEN_BEGIN(DTO)
+
+/**
+ * Message Data-Transfer-Object
+ */
+class MessageDto : public oatpp::DTO
+{
+
+    DTO_INIT(MessageDto, DTO /* Extends */)
+
+    DTO_FIELD(Int32, statusCode); // Status code field
+    DTO_FIELD(String, message);   // Message field
+};
+
+/* End DTO code-generation */
+#include OATPP_CODEGEN_END(DTO)
+
+/**
+ * Custom Request Handler
+ */
+class JsonHandler : public oatpp::web::server::HttpRequestHandler
+{
+private:
+    std::shared_ptr<oatpp::data::mapping::ObjectMapper> m_objectMapper;
+
+public:
+    /**
+   * Constructor with object mapper.
+   * @param objectMapper - object mapper used to serialize objects.
+   */
+    JsonHandler(const std::shared_ptr<oatpp::data::mapping::ObjectMapper> &objectMapper)
+        : m_objectMapper(objectMapper)
+    {
+    }
+
+    /**
+   * Handle incoming request and return outgoing response.
+   */
+    std::shared_ptr<OutgoingResponse> handle(const std::shared_ptr<IncomingRequest> &request) override
+    {
+        auto message = MessageDto::createShared();
+        message->statusCode = 1024;
+        message->message = "Hello DTO!";
+        return ResponseFactory::createResponse(Status::CODE_200, message, m_objectMapper);
+    }
+};
+
 void run()
 {
 
+    /* Create json object mapper */
+    auto objectMapper = oatpp::parser::json::mapping::ObjectMapper::createShared();
+
     /* Create Router for HTTP requests routing */
     auto router = oatpp::web::server::HttpRouter::createShared();
+
+    /* Route GET - "/hello" requests to Handler */
+    router->route("GET", "/hello", std::make_shared<Handler>());
+
+    router->route("GET", "/helloJson", std::make_shared<JsonHandler>(objectMapper /* json object mapper */));
 
     /* Create HTTP connection handler with router */
     auto connectionHandler = oatpp::web::server::HttpConnectionHandler::createShared(router);
